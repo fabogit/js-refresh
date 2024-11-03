f2752b69e7ece143ff643146033549b64f1e213f
fabo <fabo.dev.email@gmail.com>
2024-11-03 19:32:05 +0100

    #18
    refactor using switch case

--- 04_control-structures/Monster/assets/scripts/app.js
+++ 04_control-structures/Monster/assets/scripts/app.js
@@ -30,14 +30,22 @@ function writeToLog(event, value, monsterHealth, playerHealth) {
 		finalMonsterHealth: monsterHealth,
 		finalPlayerHealth: playerHealth
 	};
-	if (event === LOG_PLAYER_ATTACK) {
-		logEntry.target = "MONSTER";
-	} else if (event === LOG_PLAYER_STRONG_ATTACK) {
-		logEntry.target = "MONSTER";
-	} else if (event === LOG_MONSTER_ATTACK) {
-		logEntry.target = "PLAYER";
-	} else if (event === LOG_PLAYER_HEAL) {
-		logEntry.target = "PLAYER";
+	switch (event) {
+		case LOG_PLAYER_ATTACK:
+			logEntry.target = "MONSTER";
+			break;
+		case LOG_PLAYER_STRONG_ATTACK:
+			logEntry.target = "MONSTER";
+			break;
+		case LOG_MONSTER_ATTACK:
+			logEntry.target = "PLAYER";
+			break;
+		case LOG_PLAYER_HEAL:
+			logEntry.target = "PLAYER";
+			break;
+		default:
+			logEntry = {};
+			break;
 	}
 	battleLog.push(logEntry);
 }
