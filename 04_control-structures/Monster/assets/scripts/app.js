6be1a426313cc3106d4932380fcd289da81df2e8
fabo <fabo.dev.email@gmail.com>
2024-11-03 12:28:20 +0100

    #13
    adding bonus life button

--- 04_control-structures/Monster/assets/scripts/app.js
+++ 04_control-structures/Monster/assets/scripts/app.js
@@ -6,6 +6,7 @@ const HEAL_VALUE = 20;
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
 
@@ -22,8 +23,18 @@ function attackMonster(mode) {
 }
 
 function endRound() {
+	const initialPlayerHealth = currentPlayerHealth;
 	const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
 	currentPlayerHealth -= playerDamage;
+
+	if (currentPlayerHealth <= 0 && hasBonusLife) {
+		hasBonusLife = false;
+		removeBonusLife();
+		currentPlayerHealth = initialPlayerHealth;
+		setPlayerHealth(initialPlayerHealth);
+		alert("Bonus life used!");
+	}
+
 	if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
 		alert("You won!");
 	} else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
