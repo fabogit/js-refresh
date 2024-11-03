b08a0f475087cb151e69ea6857bc2278d293a7ef
fabo <fabo.dev.email@gmail.com>
2024-11-03 04:15:33 +0100

    #5
    refactor using if/else

--- 04_control-structures/assets/scripts/app.js
+++ 04_control-structures/assets/scripts/app.js
@@ -31,12 +31,18 @@ function calculateResult(calculationType) {
 	const enteredNumber = getUserNumberInput();
 	const initialResult = currentResult;
 	let mathOperator;
-	if (calculationType === "add") {
+	if (calculationType === "ADD") {
 		currentResult += enteredNumber;
 		mathOperator = "+";
-	} else {
+	} else if (calculationType === "SUBTRACT") {
 		currentResult -= enteredNumber;
 		mathOperator = "-";
+	} else if (calculationType === "MULTIPLY") {
+		currentResult *= enteredNumber;
+		mathOperator = "*";
+	} else if (calculationType === "DIVIDE") {
+		currentResult /= enteredNumber;
+		mathOperator = "*";
 	}
 	createAndWriteOutput(mathOperator, initialResult, enteredNumber);
 	writeToLog(calculationType, initialResult, enteredNumber, currentResult);
@@ -52,19 +58,11 @@ function subtract() {
 }
 
 function multiply() {
-	const enteredNumber = getUserNumberInput();
-	const initialResult = currentResult;
-	currentResult *= enteredNumber;
-	createAndWriteOutput("*", initialResult, enteredNumber);
-	writeToLog("MULTIPLY", initialResult, enteredNumber, currentResult);
+	calculateResult("MULTIPLY");
 }
 
 function divide() {
-	const enteredNumber = getUserNumberInput();
-	const initialResult = currentResult;
-	currentResult /= enteredNumber;
-	createAndWriteOutput("/", initialResult, enteredNumber);
-	writeToLog("DIVIDE", initialResult, enteredNumber, currentResult);
+	calculateResult("DIVIDE");
 }
 
 addBtn.addEventListener("click", add);
