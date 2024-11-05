b55fccbe3cf349aa2f9d81f5df43256a7d2ab323
fabo <fabo.dev.email@gmail.com>
2024-11-05 23:03:24 +0100

    #21
    core game logic

--- 06_functions/app.js
+++ 06_functions/app.js
@@ -4,6 +4,9 @@ const ROCK = "ROCK";
 const PAPER = "PAPER";
 const SCISSORS = "SCISSORS";
 const DEFAULT_CHOICE = ROCK;
+const RESULT_DRAW = "DRAW";
+const RESULT_PLAYER_WINS = "PLAYER WINS";
+const RESULT_COMPUTER_WINS = "COMPUTER WINS";
 
 let isGameRunning = false;
 
@@ -12,15 +15,36 @@ const getPlayerChoice = function () {
 		`${ROCK}, ${PAPER} or ${SCISSORS}?`,
 		""
 	).toUpperCase();
-	if (
-		selection !== ROCK &&
-		selection !== SCISSORS &&
-		selection !== SCISSORS
-	) {
+	if (selection !== ROCK && selection !== SCISSORS && selection !== SCISSORS) {
 		alert(`Invalid choice! We choose ${DEFAULT_CHOICE} for you`);
 		return ROCK;
 	}
 	return selection;
+};
+
+const getComputerChice = function () {
+	const randomValue = Math.random();
+	if (randomValue < 0.34) {
+		return ROCK;
+	} else if (randomValue < 0.67) {
+		return PAPER;
+	} else {
+		return SCISSORS;
+	}
+};
+
+const getWinner = function (pChoice, cChoice) {
+	if (cChoice === pChoice) {
+		return RESULT_DRAW;
+	} else if (
+		(cChoice === ROCK && pChoice === PAPER) ||
+		(cChoice === PAPER && pChoice === SCISSORS) ||
+		(cChoice === SCISSORS && pChoice === ROCK)
+	) {
+		return RESULT_PLAYER_WINS;
+	} else {
+		return RESULT_COMPUTER_WINS;
+	}
 };
 
 startGameBtn.addEventListener("click", function () {
@@ -29,6 +53,8 @@ startGameBtn.addEventListener("click", function () {
 	}
 	isGameRunning = true;
 	console.log("Game is starting...");
-	const playerSelection = getPlayerChoice();
-	console.log(playerSelection);
+	const playerChoice = getPlayerChoice();
+	const computerChoice = getComputerChice();
+	const winner = getWinner(playerChoice, computerChoice);
+	console.log(winner);
 });
