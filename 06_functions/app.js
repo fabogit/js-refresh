4f38fa6105e3a2484ac30017bf112dc0fd18ebd5
fabo <fabo.dev.email@gmail.com>
2024-11-05 22:41:01 +0100

    #20
    add user choice

--- 06_functions/app.js
+++ 06_functions/app.js
@@ -1,1 +1,34 @@
 const startGameBtn = document.getElementById("start-game-btn");
+
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_CHOICE = ROCK;
+
+let isGameRunning = false;
+
+const getPlayerChoice = function () {
+	const selection = prompt(
+		`${ROCK}, ${PAPER} or ${SCISSORS}?`,
+		""
+	).toUpperCase();
+	if (
+		selection !== ROCK &&
+		selection !== SCISSORS &&
+		selection !== SCISSORS
+	) {
+		alert(`Invalid choice! We choose ${DEFAULT_CHOICE} for you`);
+		return ROCK;
+	}
+	return selection;
+};
+
+startGameBtn.addEventListener("click", function () {
+	if (isGameRunning) {
+		return;
+	}
+	isGameRunning = true;
+	console.log("Game is starting...");
+	const playerSelection = getPlayerChoice();
+	console.log(playerSelection);
+});
