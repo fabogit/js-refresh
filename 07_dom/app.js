972ab25f3f1760899946be348953ed881f3d2fa0
fabo <fabo.dev.email@gmail.com>
2024-11-06 01:45:06 +0100

    #28
    traversing siblings

--- 07_dom/app.js
+++ 07_dom/app.js
@@ -7,26 +7,35 @@ const li = document.querySelector('li:last-of-type');
 li.textContent = li.textContent + ' (Changed!)';
 
 const body = document.body;
-const ulList = body.querySelector('ul');
-console.dir(ulList);
+const ul = body.querySelector('ul');
+// same as liFirst.parentElement
+console.dir(ul);
 
 // Childrens
-const listNodes = ulList.childNodes; // text nodes spaces and line break
-const secondListElement = ulList.children[1]; // tagged html elements
+const listNodes = ul.childNodes; // text nodes spaces and line break
+const secondListElement = ul.children[1]; // tagged html elements
 
-const firstNode = ulList.firstChild;
-const firstElem = ulList.firstElementChild;
+const firstNode = ul.firstChild;
+const firstElem = ul.firstElementChild;
 
-const lastNode = ulList.lastChild;
-const lastElem = ulList.lastElementChild;
+const lastNode = ul.lastChild;
+const lastElem = ul.lastElementChild;
 
 // Parents
 const liFirst = document.querySelector('li:first-of-type');
 liFirst.parentNode; // quite useless
 liFirst.parentElement;
 
-// any ancestor
+// any ancestor element
 liFirst.closest('body');
+
+
+// Traversing siblings
+ul.previousSibling; // any previous text node
+ul.previousElementSibling; // previous element
+
+ul.nextSibling; // any next text node
+ul.nextElementSibling; // next element
 
 // no live update just a snapshot
 // const listItemElements = document.querySelectorAll('li');
