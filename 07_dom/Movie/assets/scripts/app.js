9f3d9ca78b2f37813c81ab46f6149b43c8d23789
fabo <fabo.dev.email@gmail.com>
2024-11-10 17:17:45 +0100

    #37
    adding movies  and deleting

--- 07_dom/Movie/assets/scripts/app.js
+++ 07_dom/Movie/assets/scripts/app.js
@@ -4,6 +4,33 @@ const backdrop = document.getElementById('backdrop');
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll('input');
+const entryTextSection = document.getElementById('entry-text');
+
+const movies = [];
+
+const updateUI = () => {
+	if (movies.length === 0) {
+		entryTextSection.style.display = 'block';
+	} else {
+		entryTextSection.style.display = 'none';
+	}
+};
+
+const renderNewMovieElement = (title, imageUrl, rating) => {
+	const newMovieElement = document.createElement('li');
+	newMovieElement.className = 'movie-element';
+	newMovieElement.innerHTML = `
+    <div class="movie-element__image">
+      <img src="${imageUrl}" alt="${title}">
+    </div>
+    <div class="movie-element__info">
+      <h2>${title}</h2>
+      <p>${rating}/5 stars</p>
+    </div>
+  `;
+	const listRoot = document.getElementById('movie-list');
+	listRoot.append(newMovieElement);
+};
 
 const toggleBackdrop = () => {
 	backdrop.classList.toggle('visible');
@@ -14,22 +41,45 @@ const toggleMovieModal = () => {
 	toggleBackdrop();
 };
 
+const clearMovieInput = () => {
+	for (const usrInput of userInputs) {
+		usrInput.value = '';
+	}
+};
+
 const cancelAddMovieHandler = () => {
 	toggleMovieModal();
+	clearMovieInput();
 };
 
 const addMovieHandler = () => {
 	const titleValue = userInputs[0].value;
-	const imageValue = userInputs[1].value;
+	const imageUrlValue = userInputs[1].value;
 	const ratingValue = userInputs[2].value;
+
 	if (
 		titleValue.trim() === '' ||
-		imageValue.trim() === '' ||
+		imageUrlValue.trim() === '' ||
 		ratingValue.trim() === '' ||
-		+ratingValue < 1 || +ratingValue > 5
+		+ratingValue < 1 ||
+		+ratingValue > 5
 	) {
 		alert('Please enter valid values');
+		return;
 	}
+
+	const newMovie = {
+		title: titleValue,
+		image: imageUrlValue,
+		rating: ratingValue
+	};
+
+	movies.push(newMovie);
+	console.log(movies);
+	toggleMovieModal();
+	clearMovieInput();
+	renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+	updateUI();
 };
 
 const backdropClickHandler = () => {
