64d20a6463edff471fbcf7b8e616a198354c7453
fabo <fabo.dev.email@gmail.com>
2024-11-22 22:10:41 +0100

    #51
    WeakSet & WeakMap

--- 08_arrays/set&maps.js
+++ 08_arrays/set&maps.js
@@ -5,9 +5,13 @@ if (ids.has('Hi')) {
 	ids.delete('Hi');
 }
 
-for (const entry of ids.entries()) {
-	console.log(entry[0]);
+for (const entry of ids.values()) {
+	console.log(entry);
 }
+
+// for (const entry of ids.entries()) {
+// 	console.log(entry[0]);
+// }
 
 // MAPS
 const person1 = { name: 'Max' };
@@ -33,3 +37,21 @@ for (const value of personData.values()) {
 }
 
 console.log(personData.size);
+
+// WEAK SET AND MAP
+// less methods light on memory/gc
+let person = { name: 'Max' };
+const persons = new WeakSet();
+persons.add(person);
+
+// ... some operations
+// person = null;
+
+console.log(persons);
+
+const mapData = new WeakMap();
+mapData.set(person, 'Extra info!');
+
+person = null;
+
+console.log(mapData);
