a8072dc77a3b71d68ddf54bfe02a31f284c0dd1b
fabogit <fabo.dev.email@gmail.com>
2024-12-08 23:47:52 +0100

    #69
    getters and setters on prototypes

--- 11_prototypes-and -constructor/app.js
+++ 11_prototypes-and -constructor/app.js
@@ -9,12 +9,17 @@
 // 	constructor() {
 // 		super();
 // 		this.age = 33;
+// // Will assign to individual instances and 'recreate' the object
+// // this.method = function(){...this refers to what called the method}
+// // or this.method = () => {this will refer to the istance}
 // 	}
+// // Will assign the method to Person prototype and shared across all instances
 // 	greet() {
 // 		console.log(`Hi i am ${this.name} and i am ${this.age} years old`);
 // 	}
 // }
 
+// Constructior function
 function Person() {
 	this.age = 33;
 	this.name = 'Jesus';
@@ -36,6 +41,53 @@ console.dir(Person);
 const p = new Person();
 p.greet();
 p.printAge();
-console.log(p.__proto__);
+console.dir(p.__proto__);
+// Person.__proto__ points at the 'prototype' object
+// Person.prototype at the prototype object that will be assigned as prototype to objects created based on Person
 console.log(p.__proto__ === Person.prototype); // true
 console.log(p.__proto__ === Person.__proto__); // false
+// default prototype
+console.dir(Object.prototype);
+
+// Set and Get
+const course = {
+	// new Object()
+	title: 'JavaScript Guide',
+	rating: 5
+};
+
+// console.log(Object.getPrototypeOf(course));
+Object.setPrototypeOf(course, {
+	// ...Object.getPrototypeOf(course),
+	printRating: function () {
+		console.log(`${this.rating}/5`);
+	}
+});
+
+const student = Object.create({
+	printProgress: function () {
+		console.log(this.progress);
+	}
+}, {
+	name: {
+		configurable: true,
+		enumerable: true,
+		value: 'Max',
+		writable: true
+	}
+});
+
+// student.name = 'Max';
+
+Object.defineProperty(student, 'progress', {
+	configurable: true,
+	enumerable: true,
+	value: 0.8,
+	writable: false
+});
+
+student.printProgress();
+
+console.log(student);
+
+course.printRating();
