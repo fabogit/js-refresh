e4f3eb01a8c643fed3a33e0661bc9fef74cca862
fabo <fabo.dev.email@gmail.com>
2024-12-22 23:37:23 +0100

    #84
    Event propagation

--- 14_events/assets/scripts/events.js
+++ 14_events/assets/scripts/events.js
@@ -55,3 +55,18 @@ button.addEventListener('mouseenter', event => {
 	console.log('BUTTON');
 	console.log(event);
 });
+
+const listItems = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+
+// listItems.forEach(listItem => {
+//   listItem.addEventListener('click', event => {
+//     event.target.classList.toggle('highlight');
+//   });
+// });
+
+list.addEventListener('click', event => {
+	// console.log(event.currentTarget);
+	// event.target.classList.toggle('highlight');
+	event.target.closest('li').classList.toggle('highlight');
+});
