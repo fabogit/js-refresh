105088af4f21169764e17c969e4b58fe0b15e3e3
fabo <fabo.dev.email@gmail.com>
2024-12-29 10:48:15 +0100

    #88
    Factory Functions

--- 15_functions#2/app.js
+++ 15_functions#2/app.js
@@ -25,3 +25,18 @@ function printArray(arr) {
 	console.log(arr);
 }
 console.log(printArray(numbers));
+
+// Factory function
+function createTaxCalculator(tax) {
+	function calculateTax(amount) {
+		return amount * tax;
+	}
+
+	return calculateTax;
+}
+
+const calculateVatAmount = createTaxCalculator(0.19);
+const calculateIncomeTaxAmount = createTaxCalculator(0.25);
+
+console.log(calculateVatAmount(100));
+console.log(calculateVatAmount(200));
