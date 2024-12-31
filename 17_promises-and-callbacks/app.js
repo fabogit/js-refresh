3184e498d7c52d6336573cc71c4e5eb5317ac2ac
fabo <fabo.dev.email@gmail.com>
2024-12-31 09:21:36 +0100

    #95
    Promises then() & catch()

--- 17_promises-and-callbacks/app.js
+++ 17_promises-and-callbacks/app.js
@@ -1,29 +1,65 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
+// Promises
+somePromiseCreatingCode()
+  .then(firstResult => {
+    return 'done with first promise';
+  })
+  .catch(err => {
+    // would handle any errors thrown before
+    // implicitly returns a new promise - just like then()
+  })
+  .finally(() => {
+    // the promise is settled now - finally() will NOT return a new promise!
+    // you can do final cleanup work here
+  });
+
+
+
+const getPosition = (opts) => {
+  const promise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (success) => {
+        resolve(success);
+      },
+      (error) => {
+        reject(error);
+      },
+      opts
+    );
+  });
+  return promise;
+};
+
+const setTimer = (duration) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Done!');
+    }, duration);
+  });
+  return promise;
+};
+
 function trackUserHandler() {
-  navigator.geolocation.getCurrentPosition(
-    posData => {
-      setTimeout(() => {
-        console.log(posData);
-      }, 2000);
-    },
-    error => {
-      console.log(error);
-    }
-  );
-  setTimeout(() => {
+  let positionData;
+
+  getPosition()
+    .then((posData) => {
+      positionData = posData;
+      return setTimer(2000);
+    })
+    .catch((err) => {
+      console.log(err);
+      return 'on we go...';
+    })
+    .then((data) => {
+      console.log(data, positionData);
+    });
+  setTimer(1000).then(() => {
     console.log('Timer done!');
-  }, 0);
+  });
   console.log('Getting position...');
 }
 
 button.addEventListener('click', trackUserHandler);
-
-// let result = 0;
-
-// for (let i = 0; i < 100000000; i++) {
-//   result += i;
-// }
-
-// console.log(result);
