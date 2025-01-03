ac3d56992501146e14a32bcfba0cee029e8c7f1a
fabo <fabo.dev.email@gmail.com>
2025-01-03 13:15:35 +0100

    #111
    call api maps/api/geocode to show address

--- 24_app-share-place/src/SharePlace.js
+++ 24_app-share-place/src/SharePlace.js
@@ -1,12 +1,22 @@
 import { Modal } from './UI/Modal';
+import { Map } from './UI/Map';
+import { getCoordsFromAddress } from './Utility/Location';
 
 class PlaceFinder {
 	constructor() {
 		const addressForm = document.querySelector('form');
 		const locateUserBtn = document.getElementById('locate-btn');
 
-		locateUserBtn.addEventListener('click', this.locateUserHandler);
-		addressForm.addEventListener('submit', this.findAddressHandler);
+		locateUserBtn.addEventListener('click', this.locateUserHandler.bind(this));
+		addressForm.addEventListener('submit', this.findAddressHandler.bind(this));
+	}
+
+	selectPlace(coordinates) {
+		if (this.map) {
+			this.map.render(coordinates);
+		} else {
+			this.map = new Map(coordinates);
+		}
 	}
 
 	locateUserHandler() {
@@ -16,16 +26,19 @@ class PlaceFinder {
 			);
 			return;
 		}
-		const modal = new Modal('loading-modal-content', 'Loading location - please wait!');
+		const modal = new Modal(
+			'loading-modal-content',
+			'Loading location - please wait!'
+		);
 		modal.show();
 		navigator.geolocation.getCurrentPosition(
 			successResult => {
 				modal.hide();
 				const coordinates = {
 					lat: successResult.coords.latitude,
-					lng: successResult.coords.longitude,
+					lng: successResult.coords.longitude
 				};
-				console.log(coordinates);
+				this.selectPlace(coordinates);
 			},
 			error => {
 				modal.hide();
@@ -36,7 +49,26 @@ class PlaceFinder {
 		);
 	}
 
-	findAddressHandler() { }
+	async findAddressHandler(event) {
+		event.preventDefault();
+		const address = event.target.querySelector('input').value;
+		if (!address || address.trim().length === 0) {
+			alert('Invalid address entered - please try again!');
+			return;
+		}
+		const modal = new Modal(
+			'loading-modal-content',
+			'Loading location - please wait!'
+		);
+		modal.show();
+		try {
+			const coordinates = await getCoordsFromAddress(address);
+			this.selectPlace(coordinates);
+		} catch (err) {
+			alert(err.message);
+		}
+		modal.hide();
+	}
 }
 
 const placeFinder = new PlaceFinder();
