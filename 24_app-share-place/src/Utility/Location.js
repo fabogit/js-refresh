c25e067b00e1a21d78fca09c452386e40b0366b2
fabo <fabo.dev.email@gmail.com>
2025-01-03 15:27:49 +0100

    #112
    share position

--- 24_app-share-place/src/Utility/Location.js
+++ 24_app-share-place/src/Utility/Location.js
@@ -1,4 +1,17 @@
-const GOOGLE_API_KEY = '';
+const GOOGLE_API_KEY = 'YOUR_API_KEY';
+
+export async function getAddressFromCoords(coords) {
+  const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${coords.lat},${coords.lng}&key=${GOOGLE_API_KEY}`);
+  if (!response.ok) {
+    throw new Error('Failed to fetch address. Please try again!');
+  }
+  const data = await response.json();
+  if (data.error_message) {
+    throw new Error(data.error_message);
+  }
+  const address = data.results[0].formatted_address;
+  return address;
+}
 
 export async function getCoordsFromAddress(address) {
   const urlAddress = encodeURI(address);
