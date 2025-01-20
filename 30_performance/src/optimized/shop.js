56494531d38ed56cb7df85aee0585c6957c0d4b6
fabo <fabo.dev.email@gmail.com>
2025-01-20 01:15:56 +0100

    #119
    optimizing end

--- 30_performance/src/optimized/shop.js
+++ 30_performance/src/optimized/shop.js
@@ -2,15 +2,16 @@ import { products } from './products';
 import { renderProducts } from './rendering';
 
 function addProduct(event) {
+  event.preventDefault();
   import('./product-management.js').then(mod => {
     mod.addProduct(event);
-  });
+  })
 }
 
 function deleteProduct(productId) {
   import('./product-management.js').then(mod => {
     mod.deleteProduct(productId);
-  });
+  })
 }
 
 function initProducts() {
